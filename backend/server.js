04c5b86574f9a31dbecd966e540ea61c71e066f8
vaisakh <vaisakhtp133@gmail.com>
2021-09-01 17:43:57 +0530

    added dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,8 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
 
 const app = express();
 
@@ -12,4 +15,9 @@ app.get('/api/products/:id', (req, res) => {
   res.json(product);
 });
 
-app.listen(5000, console.log('API running successfully...'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(`API running in ${process.env.NODE_ENV} on port ${PORT}`)
+);
