748a5338f7ed4350148d2679e50942319d1acff6
vaisakh <vaisakhtp133@gmail.com>
2021-09-08 19:59:37 +0530

    Bring redux state into HomeScreen

--- frontend/src/actions/productAction.js
+++ frontend/src/actions/productAction.js
@@ -5,7 +5,7 @@ import {
 } from "../constance/productConstance"
 import axios from "axios"
 
-const listProducts = () => async (dispatch) => {
+export const listProducts = () => async (dispatch) => {
   try {
     dispatch({ type: PRODUCT_LIST_REQUEST })
     const { data } = await axios.get("api/products")
