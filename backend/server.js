48dd1198ca4f31083a1d2b9fba90a269fc96e0bc
vaisakh <vaisakhtp133@gmail.com>
2021-09-12 21:32:10 +0530

    init user login route

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import connectDB from "./config/db.js"
 import { errorHandler, notFound } from "./middileware/errorMiddleware.js"
 
 import productRoutes from "./routes/productRoutes.js"
+import userRoutes from "./routes/userRoutes.js"
 
 dotenv.config()
 
@@ -13,7 +14,10 @@ connectDB()
 
 const app = express()
 
+app.use(express.json())
+
 app.use("/api/products", productRoutes)
+app.use("/api/user", userRoutes)
 app.use(notFound)
 app.use(errorHandler)
 
