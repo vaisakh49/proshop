1518229d28ae20b2a0c46c1512901bac908fafe4
vaisakh <vaisakhtp133@gmail.com>
2021-09-17 23:35:06 +0530

    Update user profile end point added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -24,24 +24,6 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
-// @desc    Get user profile
-// @route   GET api/user/profile
-// @access  Private
-const getUserProfile = asyncHandler(async (req, res) => {
-  const user = await User.findById(req.user._id)
-  if (user) {
-    res.json({
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    })
-  } else {
-    res.status(401)
-    throw new Error("User not found")
-  }
-})
-
 // @desc    Register a user
 // @route   POST api/user
 // @access  Public
@@ -73,4 +55,49 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { registerUser, authUser, getUserProfile }
+// @desc    Get user profile
+// @route   GET api/user/profile
+// @access  Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    })
+  } else {
+    res.status(401)
+    throw new Error("User not found")
+  }
+})
+
+// @desc    Update user profile
+// @route   PUT api/user/profile
+// @access  Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    if (req.body.password) {
+      user.password = req.body.password
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error("User not found")
+  }
+})
+
+export { authUser, registerUser, getUserProfile, updateUserProfile }
