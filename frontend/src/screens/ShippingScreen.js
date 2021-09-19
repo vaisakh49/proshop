dab007cf1086a746ceaaceb5ca644497c872cabf
vaisakh <vaisakhtp133@gmail.com>
2021-09-19 16:24:32 +0530

    CheckoutSteps Component created.

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -3,6 +3,7 @@ import { Form, Button, Row, Col } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
+import CheckoutSteps from "../components/CheckoutSteps"
 import FormContainer from "../components/FormContainer"
 // import PropTypes from "prop-types"
 import { saveShippingAddress } from "../actions/cartAction"
@@ -26,6 +27,7 @@ const ShippingScreen = ({ history }) => {
 
   return (
     <FormContainer>
+      <CheckoutSteps step1 step2 />
       <h1>Shipping</h1>
       <Form onSubmit={submitHandler}>
         <Form.Group controlId="address">
