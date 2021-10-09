dc2d8a46a976ad524be5bc2e83accc8f6265f646
vaisakh <vaisakhtp133@gmail.com>
2021-10-09 23:29:41 +0530

    created orderRoutes, addOrderItem.

--- backend/server.js
+++ backend/server.js
@@ -1,31 +1,33 @@
-import colors from "colors"
-import express from "express"
-import dotenv from "dotenv"
-import connectDB from "./config/db.js"
+import colors from "colors";
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 
-import { errorHandler, notFound } from "./middileware/errorMiddleware.js"
+import { errorHandler, notFound } from "./middileware/errorMiddleware.js";
 
-import productRoutes from "./routes/productRoutes.js"
-import userRoutes from "./routes/userRoutes.js"
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.use("/api/products", productRoutes)
-app.use("/api/users", userRoutes)
-app.use(notFound)
-app.use(errorHandler)
+app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/orders", orderRoutes);
+app.use(notFound);
+app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
   console.log(
     `API running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
   )
-)
+);
